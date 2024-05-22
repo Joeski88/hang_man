c2c90018b62a4697418e6bddca357886c2e2391a
Joe Howley <joe.howley88@gmail.com>
2024-05-22 09:58:59 +0000

    turn hangman into f strings

--- run.py
+++ run.py
@@ -29,54 +29,55 @@ Hangman terminal visuals
 """
 def print_hangman(wrong):
     if(wrong == 1):
-        print("\n+=====+")
+        print(f"""\n+=====+""")
     elif(wrong == 2):
-        print("\n+====+")
-        print("       |")
-        print("       |")
-        print("       |")
+        print(f"""\n+====+
+                        |
+                        |
+                        |""")
+
     elif(wrong == 3):
-        print("\n+====+")
-        print("       |")
-        print("       |")
-        print("       |")
-        print("  ======")
+        print(f"""\n+====+
+                        |
+                        |
+                        |
+                    ======= """)
     elif(wrong == 4):
-        print("\n+====+")
-        print("  O    |")
-        print("       |")
-        print("       |")
-        print("  ======")
+        print(f"""\n+====+
+                    O   |
+                        |
+                        |
+                    ======= """)
     elif(wrong == 5):
-        print("\n+====+")
-        print("  O    |")
-        print("  |    |")
-        print("       |")
-        print("  ======")
+        print(f"""\n+====+
+                    O   |
+                    |   |
+                        |
+                    ======= """)
     elif(wrong == 6):
-        print("\n+====+")
-        print("  O    |")
-        print(" /|    |")
-        print("       |")
-        print("  ======")
+        print(f"""\n+====+
+                    O   |
+                   /|   |
+                        |
+                    ======= """)
     elif(wrong == 7):
-        print("\n+====+")
-        print("  O    |")
-        print(" /|\   |")
-        print("       |")
-        print("  ======")
+        print(f"""\n+====+
+                    O   |
+                   /|\  |
+                        |
+                    ======= """)
     elif(wrong == 8):
-        print("\n+====+")
-        print("  O    |")
-        print(" /|\   |")
-        print(" /     |")
-        print("  ======")
+        print(f"""\n+====+
+                    O   |
+                   /|\  |
+                   /    |
+                    ======= """)
     elif(wrong == 9):
-        print("\n+====+")
-        print("  O    |")
-        print(" /|\   |")
-        print(" / \   |")
-        print("  ======")
+        print(f"""\n+====+
+                    O   |
+                   /|\  |
+                   / \  |
+                    ======= """)
 
 """
 main game loop and counter
@@ -124,21 +125,21 @@ def run():
     while(True):
         ## ADDED
         if amount_of_times_wrong >= 9:
-            print(Fore.RED + "\nGAME OVER! :( ")
-            print(Fore.RED + "Your word was...")
+            print(Fore.RED + "\n    GAME OVER! :( ")
+            print(Fore.RED + "\n    Your word was...")
             print(Fore.YELLOW + randomWord)
             break
         if len(current_letters_guessed) >= 1:
-            print(Fore.YELLOW + "\nLetters guessed so far:\n ")
+            print(Fore.YELLOW + "\n Letters guessed so far:\n ")
         for letter in current_letters_guessed:
-            print(Fore.RED + letter, end=" ")
+            print(Fore.RED + letter, end="\n ")
         ### ADDED
         if current_letters_right >= length_of_word_to_guess:
-            print(Fore.CYAN + "\nYOU WONNN!")
+            print(Fore.CYAN + "\n   YOU WONNN!")
             break
 
         ### prompt for user input
-        letterGuessed = input(Fore.RED + "\nGuess a letter: \n") ######## BUGHERE color issue. makes first letter of random word change color too.
+        letterGuessed = input(Fore.RED + "\n    Guess a letter: \n") ######## BUGHERE color issue. makes first letter of random word change color too.
         
         isValid = checkValid(letterGuessed)
         if not isValid: 
@@ -192,25 +193,25 @@ def run():
 choice = ""
 
 while True:
-        print(Fore.GREEN + "\n1) Play Game")
-        print(Fore.YELLOW + "2) Rules")
-        print(Fore.RED + "3) Language Select Mode")
-        print(Fore.CYAN + "4) Multiplayer")
-        print(Fore.BLUE + "5) Exit Game\n")
+        print(Fore.GREEN + "\n  1) Play Game")
+        print(Fore.YELLOW + "   2) Rules")
+        print(Fore.RED + "  3) Language Select Mode")
+        print(Fore.CYAN + " 4) Multiplayer")
+        print(Fore.BLUE + " 5) Exit Game\n")
 
-        choice = input(Fore.GREEN + "Menu Select: \n")
+        choice = input(Fore.GREEN + "   Menu Select: \n")
         choice = choice.strip()
         if (choice == "1"):
-                print(Fore.YELLOW + Back.RED + "\nLet's Playyyyyyy!!!\n")
+                print(Fore.YELLOW + Back.RED + "\n  Let's Playyyyyyy!!!\n")
                 run()
 
         elif (choice == "2"):
                 print(Fore.CYAN + "\n1. A word is generated at random.\n2. Select desired letters. \n3. Keep guessing letters until you either guess the word or the hangman hangs!!!\n  \n------------------------------------------------------")
         elif(choice == "5"):
-                print(Fore.GREEN + "Thank you for playing!!!")
+                print(Fore.GREEN + "    Thank you for playing!!!")
                 break                
         else:
-            print(Fore.RED + "Invalid Choice, Please Try Again.")
+            print(Fore.RED + "  Invalid Choice, Please Try Again.")
 
 
 if __name__ == "__main__": 
