c0135c1a0dd0f921a1d0158f11cdc26f88878b25
Joe Howley <joe.howley88@gmail.com>
2024-05-24 13:45:15 +0000

    remove menu() function, wasnt needed

--- run.py
+++ run.py
@@ -103,9 +103,8 @@ main game loop and counter
 # counter
 def printWord(randomWord, guessedLetters):
     counter=0
-
-
     rightLetters=0
+
     for char in randomWord:
         if(char in guessedLetters):
             print(randomWord[counter], end=' ')
@@ -113,7 +112,7 @@ def printWord(randomWord, guessedLetters):
         else:
             print(" ", end=" ")
         counter += 1
-        return rightLetters
+    return rightLetters
 
 
 def printLines(randomWord):
@@ -128,9 +127,9 @@ def getWord():
     # testflag for debugging. change to true at the top of the page to use
     testWord(randomWord, TESTFLAG)
     return randomWord
+    
+    
     # Runs the game
-
-
 def run():
     while(True):
         for player in players:
@@ -138,6 +137,7 @@ def run():
             #printLines(player.randomWord)
             for x in player.randomWord:
                 # prints underscores instead of the letters of random word
+                
                 print(Fore.RED + Back.WHITE + "_", end=" ")
 
             # while loop for all possible game endings
@@ -146,13 +146,13 @@ def run():
                 print(Fore.RED + "\n GAME OVER! :( ")
                 print(Fore.RED + "\n Your word was...")
                 print(Fore.YELLOW + player.randomWord)
-                menu()
+                main()
                 break
 
             ### win state
             if player.current_letters_right >= player.length_of_word_to_guess:
                 print(Fore.CYAN + Back.GREEN + "\n YOU WONNN!")
-                menu()
+                main()
                 break
 
             if len(player.current_letters_guessed) >= 1:
@@ -174,21 +174,16 @@ def run():
             for player.current_guess_index in range(player.length_of_word_to_guess):
                 if player.randomWord[player.current_guess_index] == letterGuessed:
                     player.current_letters_guessed.append(letterGuessed)
-
                     player.current_letters_right = printWord(player.randomWord, player.current_letters_guessed)
                     # print("Right", (randomWord[current_guess_index], current_guess_index, [current_letters_right]), length_of_word_to_guess, current_letters_right >= length_of_word_to_guess)          
                     break
             # User wrong
             if (player.randomWord[player.current_guess_index] != letterGuessed):
-
                 if(player.current_letters_right < player.length_of_word_to_guess):
                     player.amount_of_times_wrong += 1
-
                     player.current_letters_guessed.append(letterGuessed)
-
                     # Update drawing
                     print_hangman(player.amount_of_times_wrong)
-
                     # Print word
                     player.current_letters_right = printWord(player.randomWord, player.current_letters_guessed)
                     testWrongGuess(player.amount_of_times_wrong, TESTFLAG)
