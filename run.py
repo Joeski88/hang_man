063ba0eb6012d50b54133b7b23ea2f5acfb8bee0
Joe Howley <joe.howley88@gmail.com>
2024-06-05 10:38:20 +0000

    fix bugs; invalid input for all menu input, multiplayer and double guessed letters

--- run.py
+++ run.py
@@ -10,7 +10,10 @@ from player import Player
 global players
 players = []
 
-punct = string.punctuation
+####################################
+punct = string.punctuation + " " # Capture whitespace.
+####################################
+
 numbers = "0123456789"
 tries = 9
 
@@ -51,18 +54,21 @@ def print_hangman(wrong):
     if(wrong == 1):
         print("+=====+")
     elif(wrong == 2):
-        print(f"""\n+====+
+        print(f"""\n
+                   +====+
                         |
                         |
                         |""")
     elif(wrong == 3):
-        print(f"""\n+====+
+        print(f"""\n
+                   +====+
                         |
                         |
                         |
                     ======= """)
     elif(wrong == 4):
-        print(f"""\n+====+
+        print(f"""\n
+                   +====+
                     O   |
                         |
                         |
@@ -128,7 +134,7 @@ def getWord():
 def run():
     while(True):
         if len(players) == 0:
-            main()
+            mainMenu()
         
         for p in range(len(players)):
 
@@ -144,16 +150,16 @@ def run():
                 print(Fore.BLACK + Back.RED + " GAME OVER! %s :( "%(current_player.name))
                 print(Fore.BLACK + Back.RED + " Your word was...")
                 print(Fore.YELLOW + current_player.randomWord)
-                #player.update_to_initial_state(self)
+                
                 players.pop(p)
                 break
 
             ### win state
             if current_player.current_letters_right >= current_player.length_of_word_to_guess:
                 print(Fore.YELLOW + Back.GREEN + " YOU WONNN!")
-                #player.update_to_initial_state(self)
-                players.pop(p)
-                main()
+                ####################################
+                mainMenu() # Call main menu
+                ####################################
                 break
 
             if len(current_player.current_letters_guessed) >= 1:
@@ -162,15 +168,21 @@ def run():
             for letter in current_player.current_letters_guessed:
                 print(Fore.RED + letter, end="  ")
 
-                # prompt for user input
+            # prompt for user input
             letterGuessed = input("\n Guess a letter (attempts left=%d): \n" %(tries - current_player.amount_of_times_wrong))
                 
             isValid = checkValid(letterGuessed)
 
-            if not isValid or letterGuessed == " ": 
+            if not isValid or letterGuessed == "": 
                 print(Fore.GREEN + "Valid characters are A-Z & a-z, No special characters allowed")
                 run()
 
+            #### Check if already guessed ######
+            if letterGuessed in current_player.current_letters_guessed:
+                print("You already guessed '%s'" %(letterGuessed))
+                run()
+            ####################################
+                
             # User is right
             for current_player.current_guess_index in range(current_player.length_of_word_to_guess):
                 if current_player.randomWord[current_player.current_guess_index] == letterGuessed:
@@ -189,13 +201,57 @@ def run():
                     testWrongGuess(current_player.amount_of_times_wrong, TESTFLAG)
 
 """ Main game menu loop """
+###### Added new function to get number of players
 
-def main():
+def getNumberOfPlayers():
+    print("Enter number of players: (1-3 max)")
+    player_num = input()
+
+    ##### Added validation checks on number of players
+    if player_num in punct or player_num == "":
+        print("Not a valid number! Please try again.")
+        getNumberOfPlayers()
+    ####################################
+        
+    player_num = int(player_num)
+
+    ##### Check number is in range of 1 to 3 and not less than 0
+    if player_num <= 0:
+        print(Fore.RED + "The min number of players is 1! Try again.")
+        getNumberOfPlayers()
+    ##### Check number is in range of 1 to 3 and not greater than 3
+    elif player_num > 3:
+        print(Fore.RED + "The max number of players is 3! Try again.")
+        getNumberOfPlayers()
+    ##############################################
+    else:        
+        print(Fore.GREEN + "Number of players:", player_num)
+        for p in range(player_num):
+            print(Fore.GREEN + "Enter player name:")
+            name = input()
+
+            #### Remove white space from name on the left and right
+            name = name.lstrip().rstrip()
+            if name == "":
+                mainMenu()
+            ##############################
+            player = addPlayer(name)
+            randomWord = getWord()
+            player.setWord(randomWord)
+
+        #### Corrected bug in multiplayer option 
+        print(Fore.GREEN + "Let's Playyyyyyy!!!")
+        
+        run() #THIS WAS INDENTED.
+        ########################################
+
+#### Refactored main() to mainMenu()
+def mainMenu():
     players = [] # reset global player array after each new game. 
     # Start menu for user
     choice = ""
    
-    print(f"""
+    print(Fore.WHITE + """
 --------------------------------------------------------------------------
              _   _    _    _   _  ____ __  __    _    _   _ _ 
             | | | |  / \\  | \\ | |/ ___|  \\/  |  / \\  | \\ | | |
@@ -204,8 +260,7 @@ def main():
             |_| |_/_/   \\_\\_| \\_|\\____|_|  |_/_/   \\_\\_| \\_(_)
 
 --------------------------------------------------------------------------""")
-    
-    
+
     while True:
         print(Fore.YELLOW + Back.MAGENTA + " 1) Play Game")
         print(Fore.YELLOW + Back.RED + " 2) Rules")        
@@ -213,37 +268,25 @@ def main():
 
         choice = input(Fore.GREEN + " Menu Select: \n")
         choice = choice.strip()
+        
+        #### Added validation to check enter key without valid input
+        if choice == "":
+            mainMenu()
+            
         if (choice == "1"):
-            
-            print("Enter number of players: (1-3 max)")
-                    
-            player_num = input()                
-            player_num = int(player_num)
-            
-            print(Fore.GREEN + "Number of players:", player_num)
-            for p in range(player_num):
-                print(Fore.GREEN + "Enter player name:")
-                name = input()
-                player = addPlayer(name)
-                randomWord = getWord()
-                player.setWord(randomWord)
-                print(Fore.GREEN + "Let's Playyyyyyy!!!")
-
-            # isValid = checkValid(player_num) 
-
-            #if not isValid or letterGuessed == " ": 
-                #print(Fore.GREEN + "Valid characters are A-Z & a-z, No special characters allowed")               
-            
-                run() #THIS WAS INDENTED.
+            #### refactored into a function
+            getNumberOfPlayers()
+            ###############################
 
         elif (choice == "2"):
-                    print(Fore.CYAN + "\n1. A word is generated at random.\n2. Select desired letters. \n3. Keep guessing letters until you either guess the word or the hangman hangs!!!\n  \n------------------------------------------------------")
+               print(Fore.CYAN + "\n1. A word is generated at random.\n2. Select desired letters. \n3. Keep guessing letters until you either guess the word or the hangman hangs!!!\n  \n------------------------------------------------------")
         elif(choice == "3"):
-                    print(Fore.GREEN + " Thank you for playing!!!")
-                    break                
+               print(Fore.GREEN + " Thank you for playing!!!")
+               break                
         else:
-                print(Fore.RED + " Invalid Choice, Please Try Again.")
+               print(Fore.RED + " Invalid Choice, Please Try Again.")
 
 
-if __name__ == "__main__": 
-    main()
+if __name__ == "__main__":
+    #### Refactored main() to mainMenu()
+    mainMenu()
